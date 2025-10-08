c7ae54850bd26447d3336349769cfa19c1d413f6
Diogo Costa <al.916492@aeaav.pt>
2025-10-08 17:00:14 +0100

    commit

---
+++ TrabGrupoM17C_Pert.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] n = new int[10];
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Número: ");
+            n[i] = int.Parse(Console.ReadLine());
+        }
+
+        int maior = n[0];
+        int pos = 0;
+
+        for (int i = 1; i < 10; i++)
+        {
+            if (n[i] > maior)
+            {
+                maior = n[i];
+                pos = i;
+            }
+        }
+
+        Console.WriteLine("Maior número: {0}, índice: {1}.", maior, pos);
+    }
+}
